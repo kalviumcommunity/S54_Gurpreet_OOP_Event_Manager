7d93f2f29484c17411f46118b0e8b9a7f7f94807
Gurpreet Singh Ghuman <142794541+Max3796@users.noreply.github.com>
2024-09-03 20:41:53 +0530

    Revert "Applied the concept of array of objects"

--- EventManager/src/Main.java
+++ EventManager/src/Main.java
@@ -1,10 +1,27 @@
-package com.eventmanagement;
-
-import com.eventmanagement.controller.EventController;
+import model.Meeting;
+import model.Party;
 
 public class Main {
     public static void main(String[] args) {
-        EventController controller = new EventController();
-        controller.run();  // Start the event management process
+        Meeting meeting = new Meeting("Team Meeting", "2024-09-10", "Conference Room A", "Project Updates");
+        Party party = new Party("Office Party", "2024-09-12", "Rooftop", 50);
+
+        
+        System.out.println("Meeting Details:");
+        System.out.println("Name: " + meeting.getName());
+        System.out.println("Date: " + meeting.getDate());
+        System.out.println("Location: " + meeting.getLocation());
+        System.out.println("Agenda: " + meeting.getAgenda());
+        meeting.scheduleEvent(); 
+
+        System.out.println(); 
+
+       
+        System.out.println("Party Details:");
+        System.out.println("Name: " + party.getName());
+        System.out.println("Date: " + party.getDate());
+        System.out.println("Location: " + party.getLocation());
+        System.out.println("Number of Guests: " + party.getNumberOfGuests());
+        party.scheduleEvent();  
     }
 }
