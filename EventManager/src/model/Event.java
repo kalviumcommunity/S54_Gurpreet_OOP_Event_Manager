1447f17be0fdc94a5e62e4d1b742eae33b8c4b4b
max3796 <gps2005.singh@gmail.com>
2024-09-03 14:10:08 +0530

    Added Project classes and used this keyword

---
+++ EventManager/src/model/Event.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Event {
+    private String name;
+    private String date;
+    private String location;
+
+    public Event(String name, String date, String location) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void scheduleEvent() {
+        System.out.println("Scheduling event: " + name + " on " + date + " at " + location);
+    }
+}
