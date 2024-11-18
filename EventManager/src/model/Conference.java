c89bb6dd5e2734a26e35a79bdb95ff2ed64e4399
max3796 <gps2005.singh@gmail.com>
2024-11-18 09:33:30 +0530

    completed milestone-4

--- EventManager/src/model/Conference.java
+++ EventManager/src/model/Conference.java
@@ -1,20 +1,45 @@
 package model;
 
+// Single Inheritance
 public class Conference extends Event {
     private String keynoteSpeaker;
 
+    // Constructor
     public Conference(String name, String date, String location, String keynoteSpeaker) {
         super(name, date, location);
         this.keynoteSpeaker = keynoteSpeaker;
     }
 
+    // Getter and Setter
     public String getKeynoteSpeaker() {
         return keynoteSpeaker;
     }
 
+    public void setKeynoteSpeaker(String keynoteSpeaker) {
+        this.keynoteSpeaker = keynoteSpeaker;
+    }
+
+    // Overriding the scheduleEvent method (Virtual Function)
     @Override
     public void scheduleEvent() {
-        super.scheduleEvent();
+        super.scheduleEvent(); // Call the parent class method
         System.out.println("Conference keynote speaker: " + keynoteSpeaker);
+    }
+
+    // Overloading scheduleEvent with additional parameters
+    public void scheduleEvent(String priority, int durationInHours) {
+        System.out.println("Scheduling conference: " + getName());
+        System.out.println("Priority: " + priority);
+        System.out.println("Duration: " + durationInHours + " hours");
+        System.out.println("Keynote Speaker: " + keynoteSpeaker);
+    }
+
+    // Overriding the eventDetails method from Event (Abstraction)
+    @Override
+    public void eventDetails() {
+        System.out.println("Conference Name: " + getName());
+        System.out.println("Date: " + getDate());
+        System.out.println("Location: " + getLocation());
+        System.out.println("Keynote Speaker: " + keynoteSpeaker);
     }
 }
--- EventManager/src/model/Party.java
+++ EventManager/src/model/Party.java
@@ -1,20 +1,45 @@
 package model;
 
+// Single Inheritance
 public class Party extends Event {
     private int numberOfGuests;
 
+    // Constructor
     public Party(String name, String date, String location, int numberOfGuests) {
         super(name, date, location);
         this.numberOfGuests = numberOfGuests;
     }
 
+    // Getter and Setter
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
 
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    // Overriding the scheduleEvent method (Virtual Function)
     @Override
     public void scheduleEvent() {
-        super.scheduleEvent();
+        super.scheduleEvent(); // Call the parent class method
         System.out.println("Party guest count: " + numberOfGuests);
+    }
+
+    // Overriding the eventDetails method from Event (Abstraction)
+    @Override
+    public void eventDetails() {
+        System.out.println("Party Name: " + getName());
+        System.out.println("Date: " + getDate());
+        System.out.println("Location: " + getLocation());
+        System.out.println("Number of Guests: " + numberOfGuests);
+    }
+
+    // Overloading the scheduleEvent method for additional details
+    public void scheduleEvent(boolean isOutdoor, String theme) {
+        String eventType = isOutdoor ? "Outdoor Party" : "Indoor Party";
+        System.out.println("Scheduling " + eventType + ": " + getName());
+        System.out.println("Theme: " + theme);
+        System.out.println("Guest Count: " + numberOfGuests);
     }
 }
